d991025e0e8cd37b05934e710607ea8053990db2
Standa Geidl <standag@gmail.com>
2024-05-17 10:44:39 +0200

    update May 2024

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,17 +3,21 @@ if status is-interactive
 end
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
-set -gx LDFLAGS "-L/opt/homebrew/lib"
-set -gx RUSTFLAGS "-L/opt/homebrew/lib"
-set -gx CPPFLAGS "-L/opt/homebrew/include"
-set -gx CFLAGS "-L/opt/homebrew/include"
+set -gx LDFLAGS -L/opt/homebrew/lib
+set -gx RUSTFLAGS -L/opt/homebrew/lib
+set -gx CPPFLAGS -L/opt/homebrew/include
+set -gx CFLAGS -L/opt/homebrew/include
+set -gx CLOUDSDK_PYTHON python3.9
 
 alias k kubectl
 alias z zellij
+alias nomex "poetry run nomex"
 
 fish_add_path -a /usr/local/opt/python@3.7/bin
 fish_add_path -a /usr/local/opt/python@3.8/bin
 fish_add_path -a /usr/local/opt/python@3.10/bin
+fish_add_path -a /Users/standag/.modular/pkg/packages.modular.com_mojo/bin
+fish_add_path -p /Users/standag/.asdf/shims
 
 fish_add_path -a ~/.cargo/bin
 
@@ -23,9 +27,8 @@ fish_add_path -a /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
 # rancher desktop binaries: docker, kubectl, ... 
 fish_add_path -a ~/.rd/bin
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/standag/.ghcup/bin $PATH # ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+set -gx PATH $HOME/.cabal/bin /Users/standag/.ghcup/bin $PATH # ghcup-env
 
-
-# Setting PATH for Python 3.12
-# The original version is saved in /Users/standag/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$PATH"
+# opam configuration
+source /Users/standag/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
