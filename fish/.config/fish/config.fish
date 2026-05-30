31dbf3a7e4d2933cb5cacd36e28006d92c2ad11b
Stanislav Geidl <standag@gmail.com>
2026-05-30 10:44:39 +0200

    chore: update 05/2026

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,21 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if test -z $IN_NIX_SHELL
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    # set -gx LDFLAGS -L/opt/homebrew/lib
-    # set -gx RUSTFLAGS -L/opt/homebrew/lib
-    # set -gx CPPFLAGS -L/opt/homebrew/include
-    # set -gx CFLAGS -L/opt/homebrew/include
-end
-
+eval "$(/opt/homebrew/bin/brew shellenv)"
 set -gx CLOUDSDK_PYTHON python3.9
-
 set -gx EDITOR hx
 
 alias k kubectl
 # alias z zellij
-alias nomex "poetry run nomex"
+alias cc /opt/homebrew/bin/claude
 
 # fish_add_path -a /usr/local/opt/python@3.7/bin
 # fish_add_path -a /usr/local/opt/python@3.8/bin
@@ -29,17 +21,24 @@ alias nomex "poetry run nomex"
 set -gx USE_GKE_GCLOUD_AUTH_PLUGIN True
 fish_add_path -a /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
 
-# rancher desktop binaries: docker, kubectl, ... 
-fish_add_path -a ~/.rd/bin
-
 # set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 # set -gx PATH $HOME/.cabal/bin /Users/standag/.ghcup/bin $PATH # ghcup-env
 
 # opam configuration
 # source /Users/standag/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
 
-# uv
-fish_add_path "/Users/standag/.local/bin"
+# rancher desktop binaries: docker, kubectl, ... 
+fish_add_path -a ~/.rd/bin
+
+# uv tools
+fish_add_path -a ~/.local/bin
 
 zoxide init fish | source
 starship init fish | source
+fzf --fish | source
+fnm env --use-on-cd --version-file-strategy=recursive --shell fish | source
+
+set -gx MISE_ACTIVATE_AGGRESSIVE 1
+mise activate fish | source
+
+set -gx JIRA_TOKEN $(security find-generic-password -s "dx-jira-token" -w)
